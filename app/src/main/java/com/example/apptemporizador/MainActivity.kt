edf23e9dd1edfdee4ceb87f7afde9c9edd419e47
Felipe <felipetermometria@hotmail.com>
2023-11-20 15:38:34 -0300

    insert button Start winth clickListener

--- app/src/main/java/com/example/apptemporizador/MainActivity.kt
+++ app/src/main/java/com/example/apptemporizador/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.apptemporizador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText: EditText = findViewById(R.id.edit_value)
+        val buttonStart:   Button = findViewById(R.id.btn_start)
+        val buttonStop:   Button = findViewById(R.id.btn_stop)
+        val result: TextView = findViewById(R.id.txt_result)
+
+
+        buttonStart.setOnClickListener {
+           Toast.makeText(this, "Teste", Toast.LENGTH_SHORT).show()
+
+        }
     }
+
 }
